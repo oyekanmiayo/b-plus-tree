add35543abf90a49f7719d8c5ef02051e3a069c5
Ayomide Oyekanmi <ayomideoyekanmi@gmail.com>
2024-03-18 18:08:04 +0000

    Implement a partially working node split

--- v2/btree.go
+++ v2/btree.go
@@ -113,7 +113,7 @@ func (btree *BTree) nodeInsert(node *BTreeNode, key int, value []byte) *BTreeNod
 		// This means any new inserts will cause an overflow (and trigger a split)
 		if node.keysOccupied == btree.degree-1 {
 			retNode = btree.newBTreeNodeItem(key, value)
-			btreeNodeSplit(btree, node, retNode, insertPos)
+			btree.nodeSplit(node, retNode, insertPos)
 
 		} else {
 			// we can insert in current node without a violation
@@ -134,7 +134,7 @@ func (btree *BTree) nodeInsert(node *BTreeNode, key int, value []byte) *BTreeNod
 
 		if retNode != nil {
 			if node.keysOccupied == btree.degree-1 { // We will have an overflow, so split now
-				btreeNodeSplit(btree, node, retNode, insertPos)
+				btree.nodeSplit(node, retNode, insertPos)
 			} else {
 				// This always assumes that keysOccupied < degree :)
 				for k := insertPos; k <= node.keysOccupied; k++ {
@@ -161,8 +161,78 @@ func (btree *BTree) nodeInsert(node *BTreeNode, key int, value []byte) *BTreeNod
 	return retNode
 }
 
-func btreeNodeSplit(btree *BTree, node, retNode *BTreeNode, insertPos int) {
+func (btree *BTree) nodeSplit(node, retNode *BTreeNode, insertPos int) {
+	hasChildren := node.noOfExistingChildren != 0
+	tempNode := btree.newBTreeNode()
+	tempNode.children = append(tempNode.children, retNode.children...)
 
+	// somehow, minDegree also doubles as the average?
+	treeAvg := btree.minDegree
+
+	if insertPos < btree.minDegree-1 {
+
+		// save KV pair for the media key that will be up-shifted to tempNode
+		moveKeyValBetweenNodes(node, treeAvg-2, tempNode, 0)
+
+		// move all KV pairs in the overflowing node to the right by one up to the t-2 index
+		for idx := treeAvg - 2; idx > insertPos; idx-- {
+			moveKeyValBetweenNodes(node, idx-1, node, idx)
+		}
+
+		// there's now free space at insertPos, so insert the new KV pair there
+		moveKeyValBetweenNodes(retNode, 0, node, insertPos)
+
+	} else if insertPos > btree.minDegree-1 {
+
+		// save KV pair for the media key that will be up-shifted to tempNode
+		moveKeyValBetweenNodes(node, treeAvg-1, tempNode, 0)
+
+		for idx := treeAvg - 1; idx < insertPos && idx < node.keysOccupied-1; idx++ {
+			// node[idx] = node[idx+1]
+			moveKeyValBetweenNodes(node, idx+1, node, idx)
+		}
+
+		// there's now free space at insertPos, so insert the new KV pair there
+		// insertPos - 1 because the position would be left shifted after removing the middle KV pair
+		moveKeyValBetweenNodes(retNode, 0, node, insertPos-1)
+	} else {
+		moveKeyValBetweenNodes(retNode, 0, tempNode, 0)
+	}
+
+	// So now we have insert the KV at the proper position and put the KV to be upshifted in tempNode
+	/**
+	- What next?
+	- move node to be upshifted to retNode
+	- retNode's left child will be the old root
+	- retNode's right (next) child will be the right handside of the old root
+	*/
+
+	moveKeyValBetweenNodes(tempNode, 0, retNode, 0)
+	retNode.children[0] = node
+	retNode.children[1] = btree.newBTreeNode()
+	for idx := treeAvg - 1; idx < btree.degree-1; idx++ {
+		// insert KV pairs from KV after upshifted KV to the end in the retNode.children[1].keys[0 -> treeAvg - 1]
+		// idx-btree.degree+1 is the calculation that helps us insert in the right index
+		moveKeyValBetweenNodes(node, idx, retNode.children[1], idx-treeAvg+1)
+		node.keys[idx] = math.MaxInt
+		node.values[idx] = nil
+	}
+
+	if hasChildren {
+		if insertPos < treeAvg-1 {
+			for idx := treeAvg - 1; idx < btree.degree; idx++ {
+				// insert rhs children from overflowing node to children of the new rhs node of retNode
+				retNode.children[1].children[idx-treeAvg+1] = retNode.children[0].children[idx]
+			}
+
+			for idx := treeAvg - 1; idx > insertPos; idx-- {
+
+			}
+
+		} else { //  insertPos >= treeAvg - 1
+
+		}
+	}
 }
 
 func moveKeyValBetweenNodes(fNode *BTreeNode, fIdx int, sNode *BTreeNode, sIdx int) {
