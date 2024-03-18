ca84c321ff77307a5764406de9f71f8afabbdda1
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-03-18 18:44:37 +0100

    add more tests for  debugging
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- v3/btree_test.go
+++ v3/btree_test.go
@@ -4,12 +4,59 @@ import (
 	"testing"
 )
 
-func TestInsert(t *testing.T) {
-	tree := newBTree(3)
+func TestInsertRoot(t *testing.T) {
+	tree := newBTree(2)
 
 	tree.insert(1, []byte("hello"))
-	tree.insert(2, []byte("hello"))
-	tree.insert(3, []byte("hello"))
-	tree.insert(4, []byte("hello"))
-	tree.insert(5, []byte("hello"))
 }
+
+func TestInsertAfterSplit(t *testing.T) {
+	tree := newBTree(3)
+
+	for i := 1; i <= 10; i++ {
+		tree.insert(i, []byte("hello"))
+	}
+}
+
+/*
+// maybe bench the bin search vs a linear
+func TestFind(t *testing.T) {
+	tree := newBTree(3)
+
+	for i := 1; i <= 10; i++ {
+		tree.insert(i, []byte("hello"))
+	}
+
+	fmt.Println("? wtf")
+
+	for i := 1; i <= 10; i++ {
+		res := tree.find(i)
+
+		if !bytes.Equal(res, []byte("hello")) {
+			t.Error("could not find node")
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+	tree := newBTree(3)
+
+	for i := 1; i <= 10; i++ {
+		tree.insert(i, []byte("hello"))
+	}
+
+	for i := 1; i <= 10; i++ {
+		tree.delete(i)
+	}
+
+	for i := 1; i <= 10; i++ {
+		res := tree.find(i)
+
+		if res != nil {
+			return t.Error("did not remove node correctly")
+		}
+	}
+
+}
+
+*/
