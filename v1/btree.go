959f4c54a9e632ea29f0a84e2955bade0ab0434a
Ayomide Oyekanmi <ayomideoyekanmi@gmail.com>
2024-03-19 08:54:48 +0000

    Add offset functions and tests

--- v1/btree.go
+++ v1/btree.go
@@ -5,12 +5,6 @@ import "encoding/binary"
 /*
 BNode
 
-# Assuming we need only 2 bytes to store integers
-
-BNode consists of:
-| type | nkeys |  pointers  |   offsets  | key-values
-|  2B  |   2B  | nkeys * 8B | nkeys * 2B | ...
-
 This is the format of the KV pair. Lengths followed by data.
 | klen | vlen | key | val |
 |  2B  |  2B  | ... | ... |
@@ -21,9 +15,33 @@ type BNode struct {
 	data []byte
 }
 
+/*
+
+- Store sizes of each part of the node (page)
+- unit is bytes
+- this is not to be confused with their actual values.
+- this simply stores the size within with the values must be contained
+
+BNode consists of:
+| type | nkeys |  pointers  |   offsets  | key-values
+|  2B  |   2B  | nkeys * 8B | nkeys * 2B | ...
+*/
+
 const (
-	BNODE_NODE = 1 // internal node (no values)
-	BNODE_LEAF = 2 // leaf node (has values)
+	BNODE_TYPE   = 2
+	BNODE_NKEYS  = 2
+	BNODE_HEADER = BNODE_TYPE + BNODE_NKEYS
+
+	// BNODE_POINTER_SIZE and BNODE_OFFSET_SIZE represent the size of one pointer
+	// BNODE_HEADER is different because there's only one type and nkeys
+	// so the size of one of each covers the total size
+	BNODE_POINTER_SIZE = 8
+	BNODE_OFFSET_SIZE  = 2
+)
+
+const (
+	BNODE_NODE = iota // internal node (no values)
+	BNODE_LEAF        // leaf node (has values)
 )
 
 type BTree struct {
@@ -35,9 +53,13 @@ type BTree struct {
 	del func(uint642 uint64)
 }
 
+// NewBNode
+// BType - Node type is BNODE_NODE or BNODE_LEAF
+// NKeys - number of keys this node can store
 func NewBNode(bType, nKeys int) BNode {
+	nodeInitialSize := BNODE_HEADER + (BNODE_POINTER_SIZE * nKeys) + (BNODE_OFFSET_SIZE * nKeys)
 	node := BNode{
-		data: make([]byte, 4),
+		data: make([]byte, nodeInitialSize),
 	}
 
 	// node type
@@ -49,10 +71,54 @@ func NewBNode(bType, nKeys int) BNode {
 	return node
 }
 
-func (node BNode) bType() uint16 {
+func (node BNode) BType() uint16 {
 	return binary.LittleEndian.Uint16(node.data[0:2])
 }
 
-func (node BNode) nKeys() uint16 {
+func (node BNode) NKeys() uint16 {
 	return binary.LittleEndian.Uint16(node.data[2:4])
+}
+
+func (node BNode) SetPtr(idx uint16, val uint64) {
+	pos := BNODE_HEADER + (idx * BNODE_POINTER_SIZE)
+	binary.LittleEndian.PutUint64(node.data[pos:], val)
+}
+
+func (node BNode) GetPtr(idx uint16) uint64 {
+	if idx >= node.NKeys() {
+		panic("idx >= NKeys")
+	}
+
+	// this gets us to the starting point of the bytes for this pointer
+	pos := BNODE_HEADER + (idx * BNODE_POINTER_SIZE)
+
+	// read 8 bytes from starting point (pos)
+	return binary.LittleEndian.Uint64(node.data[pos:])
+}
+
+// The offset of the first KV is always zero, so it's not stored in the list
+// offsetPos(node, idx) will fetch the starting point of the offset position for a particular index
+func offsetPos(node BNode, idx uint16) uint16 {
+	return BNODE_HEADER + (BNODE_POINTER_SIZE * BNODE_NKEYS) + (BNODE_OFFSET_SIZE * (idx - 1))
+}
+
+func (node BNode) SetOffset(idx, offset uint16) {
+	if idx == 0 && offset == 0 {
+		return
+	}
+
+	if idx == 0 {
+		panic("idx is 0, but offset isn't 0")
+	}
+
+	binary.LittleEndian.PutUint16(node.data[offsetPos(node, idx):], offset)
+}
+
+func (node BNode) GetOffset(idx uint16) uint16 {
+	// The offset of the first KV is always zero, so it's not stored in the list
+	if idx == 0 {
+		return 0
+	}
+
+	return binary.LittleEndian.Uint16(node.data[offsetPos(node, idx):])
 }
--- v1/btree_test.go
+++ v1/btree_test.go
@@ -6,14 +6,62 @@ import (
 
 func TestNewBNode(t *testing.T) {
 	node := NewBNode(BNODE_LEAF, 5)
-	typeUInt16 := node.bType()
-	nKeysUInt16 := node.nKeys()
+	typeUInt16 := node.BType()
+	nKeysUInt16 := node.NKeys()
 
 	if typeUInt16 != uint16(BNODE_LEAF) {
 		t.Errorf("Node type mismatch. Expected: %d, Got: %d", uint16(BNODE_LEAF), typeUInt16)
 	}
 
 	if nKeysUInt16 != uint16(5) {
-		t.Errorf("Node nKeys mismatch. Expected: %d, Got: %d", uint16(5), nKeysUInt16)
+		t.Errorf("Node NKeys mismatch. Expected: %d, Got: %d", uint16(5), nKeysUInt16)
+	}
+
+	node.SetPtr(0, uint64(10))
+	node.SetPtr(2, uint64(250))
+
+	getPtr0 := node.GetPtr(0)
+	getPtr2 := node.GetPtr(2)
+
+	if getPtr0 != uint64(10) {
+		t.Errorf("Node Ptr mismatch at idx %d. Expected: %d, Got: %d.", 0, uint64(10), getPtr0)
+	}
+
+	if getPtr2 != uint64(250) {
+		t.Errorf("Node Ptr mismatch at idx %d. Expected: %d, Got: %d.", 2, uint64(250), getPtr2)
+	}
+}
+
+func TestBNode_SetPtr_GetPtr(t *testing.T) {
+	node := NewBNode(BNODE_LEAF, 5)
+	node.SetPtr(0, uint64(10))
+	node.SetPtr(2, uint64(250))
+
+	getPtr0 := node.GetPtr(0)
+	getPtr2 := node.GetPtr(2)
+
+	if getPtr0 != uint64(10) {
+		t.Errorf("Node Ptr mismatch at idx %d. Expected: %d, Got: %d.", 0, uint64(10), getPtr0)
+	}
+
+	if getPtr2 != uint64(250) {
+		t.Errorf("Node Ptr mismatch at idx %d. Expected: %d, Got: %d.", 2, uint64(250), getPtr2)
+	}
+}
+
+func TestBNode_SetOffset_GetOffset(t *testing.T) {
+	node := NewBNode(BNODE_NODE, 5)
+
+	// Test that the first offset is always 0
+	node.SetOffset(0, 0)
+	offset0 := node.GetOffset(0)
+	if offset0 != uint16(0) {
+		t.Errorf("Node Offset mismatch at idx %d. Expected: %d, Got: %d.", 0, uint64(0), offset0)
+	}
+
+	node.SetOffset(1, 20)
+	offset20 := node.GetOffset(1)
+	if offset20 != uint16(20) {
+		t.Errorf("Node Offset mismatch at idx %d. Expected: %d, Got: %d.", 0, uint64(20), offset20)
 	}
 }
