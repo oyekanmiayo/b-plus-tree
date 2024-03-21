99e6d295dac197886a156a67a914e5b6558ad01a
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-03-21 17:46:40 +0100

    stub delete
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- v3/examples/search/main.go
+++ v3/examples/search/main.go
@@ -3,6 +3,9 @@ package main
 func main() {
 	// the keys are: 1, 2, 3, 4, 5, 6, 7, 8
 	// this is pre-populated
+	// this is a simple 2-way b-tree
+	// factoid: this is a 2-3 tree, B-Trees generalise 2-3 Trees
+	// this example will break with a bigger degree, it's just to show the operations
 	BasicSearchExample(6)
 	BasicSearchLeaf(6)
 }
