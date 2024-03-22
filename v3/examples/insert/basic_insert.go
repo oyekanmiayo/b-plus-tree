f21923ba42ad597211093988f23f3d7c41f0d879
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-03-22 08:12:11 +0100

    psuedo code explainer insert
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- v3/examples/insert/basic_insert.go
+++ v3/examples/insert/basic_insert.go
@@ -27,13 +27,54 @@ func (n *Node) search(key int) (*Node, int, error) {
 }
 
 func (n *Node) insert(key int) error {
+	// locate target leaf
+	// append value
+
+	// case 1
+	// does the target node have room? if yes do nothing
+	// case 2
+	// is it a leaf node? the bound: len(n.pointers) < n + 1
+	// is it an internal node? the bound is len(n.keys) < n
+	// where n >= 2, n == max degree == occupancy == fanout
+
 	return nil
 }
 
 func BasicInsertExample() {
 	// our root node is special, we store keys and store pointers to
 	// data records inside it at first, when we split our root for the first time
-	// we 'recurse' 'up' pushing a new internal node upward, rearranging our pointers
+	// we 'recurse' 'UP' pushing a new internal node upward, rearranging our pointers
+	// then it 'loses' its borrow leaf capabilities.
+
+	// B-Trees are built in-reverse of a classic binary search tree.
+	// (new internal) ....can add more nodes until full, split, recurse.
+	//   \               /
+	//   (root)
+
+	// splitting:
+	// step one: oh crap I'm a full root node, I need to give away my keys!
+	//  (nil)            (nil)
+	//   \               /
+	//   (root )
+
+	// every node except the root node must respect the inquality:
+	// branching factor - 1 <= num keys < (2 * branching factor) - 1
+	// if this doesn't make sense ignore it. The take away:
+	// every node except root has a minimum no. of keys or it's invalid.
+
+	// step two: it's promotion time, split keys into two halves, if there's only halve,
+	// ie the new node has < MIN_KEYS we push up one-way:
+	// lastly point to data/allocate/move root node's data to a leaf.
+
+	// -- LEAF
+	//  (internal node)  (internal or nil)
+	//   \               /
+	//   (root)
+
+	// step three, check all children:
+	// recurse DOWN from root to new internal node(s), check that we're not full
+	// if full, we split again on internal node, allocate a new node(s)
+
 	root := &Node{}
 
 	for key := 1; key <= 8; key++ {
