fb756b279d15ed83d1d16fce2effcc656e379719
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-04-15 12:50:56 +0100

    fix: bad index reclaimation in deletion
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- v3/delete.go
+++ v3/delete.go
@@ -24,8 +24,8 @@ func (n *Node) delete(t *BTree, key int) error {
 
 	// is the leaf empty or underflown?
 	if n.kind == LEAF_NODE && len(n.data) < (MAX_DEGREE/2) {
-		if sibling, idx, err := n.preMerge(); err == nil {
-			return n.mergeSibling(t, sibling, idx, key)
+		if sibling, _, err := n.preMerge(); err == nil {
+			return n.mergeSibling(t, sibling, key)
 		} else {
 			return errors.New("see rebalancing.go")
 		}
@@ -46,8 +46,8 @@ func (n *Node) delete(t *BTree, key int) error {
 	// underflow triggers a merge cascade recurse to parent
 	// recurse UPWARD and check invariants
 	if len(n.parent.keys) < ((MAX_DEGREE - 1) / 2) {
-		if sibling, idx, err := n.parent.preMerge(); err == nil {
-			return n.parent.mergeSibling(t, sibling, idx, key)
+		if sibling, _, err := n.parent.preMerge(); err == nil {
+			return n.parent.mergeSibling(t, sibling, key)
 		} else {
 			return errors.New("see rebalancing.go")
 		}
@@ -60,7 +60,6 @@ func (n *Node) delete(t *BTree, key int) error {
 // https://github.com/rust-lang/rust/blob/1c19595575968ea77c7f85e97c67d44d8c0f9a68/library/alloc/src/collections/btree/merge_iter.rs#L41
 // and maybe... just maybe, stream/lift that iter out to a scheduler/async runtime -- complex, magical, do not do this, but neat to know.
 // NB/Warning if you want to do it anyway: You need to be careful when providing a cursor/iter api that it is re-entrant & thread safe.
-// Doing this may likely lead to needing to do _unsafe memory things_ carefully consider the invariants.
 
 // go/pebble
 // iterator/cursor: https://github.com/cockroachdb/pebble/blob/c4daad9128e053e496fa7916fda8b6df57256823/internal/manifest/btree.go#L973 &&
@@ -73,7 +72,7 @@ func (n *Node) delete(t *BTree, key int) error {
 contents should be merged. if their contents do not fit into a single node
 else are redistributed - rebalancing.go.
 */
-func (n *Node) mergeSibling(t *BTree, sibling *Node, idx, key int) error {
+func (n *Node) mergeSibling(t *BTree, sibling *Node, key int) error {
 	switch n.kind {
 	case LEAF_NODE:
 		assertCommonParent(n, sibling)
@@ -91,8 +90,8 @@ func (n *Node) mergeSibling(t *BTree, sibling *Node, idx, key int) error {
 				sibling.parent.keys = cut(i, sibling.parent.keys)
 
 				if len(n.parent.keys) < int(math.Ceil(float64(MAX_DEGREE)/2)) {
-					if sibling, idx, err := sibling.parent.preMerge(); err == nil {
-						return n.parent.mergeSibling(t, sibling, idx, key)
+					if sibling, _, err := sibling.parent.preMerge(); err == nil {
+						return n.parent.mergeSibling(t, sibling, key)
 					} else {
 						return errors.New("see rebalancing.go")
 					}
@@ -105,15 +104,18 @@ func (n *Node) mergeSibling(t *BTree, sibling *Node, idx, key int) error {
 		sibling.keys = append(sibling.keys, n.keys...)
 		sibling.children = append(sibling.children, n.children...)
 
-		// todo don't pass along idx
-		// TODO: fix this
 		// mark n for deallocation
-		n.parent.children = append(n.parent.children[:idx+1], n.parent.children[idx+2:]...)
+		for i, child := range n.parent.children {
+			if child == n {
+				n.parent.children = append(n.parent.children[:i], n.parent.children[i+1:]...)
+				break
+			}
+		}
 
 		// recursive case
 		if len(n.parent.children) < int(math.Ceil(float64(MAX_DEGREE)/2)) {
-			if sibling, idx, err := n.parent.preMerge(); err == nil {
-				return n.parent.mergeSibling(t, sibling, key, idx)
+			if sibling, _, err := n.parent.preMerge(); err == nil {
+				return n.parent.mergeSibling(t, sibling, key)
 			} else {
 				return errors.New("see rebalancing.go")
 			}
@@ -163,10 +165,10 @@ func (n *Node) preMerge() (*Node, int, error) {
 		}
 
 	case ROOT_NODE:
-		// if len(n.keys)+len(n.children[0].keys) <= MAX_DEGREE {
 		// if underfull merge with first left child
-		return n.children[0], 0, nil
-
+		if len(n.keys)+len(n.children[0].keys) <= MAX_DEGREE {
+			return n.children[0], 0, nil
+		}
 	}
 
 	return nil, 0, errors.New("cannot merge with sibling")
@@ -184,16 +186,16 @@ func MergeDelete(tree *BTree) {
 
 func MergeDeleteExample(tree *BTree) {
 	tree.Delete(4)
-	tree.Delete(2)
+	//tree.Delete(2)
 	/*
 		NOTE: this example doesn't look 1:1
-		because the "steal" sibling optimisation is not implemented
+		because the "steal" sibling/redistribution/rebalance optimisation is not implemented
 		and an underflow always forces a merge
 		see: rebalancing.go
 	*/
 	fmt.Println(tree.root)
 	fmt.Println(tree.root.children[0])
-	fmt.Println(tree.root.children[1])
+	//fmt.Println(tree.root.children[1])
 	//fmt.Println(tree.root.children[2])
 }
 
